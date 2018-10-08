d2ee5f0e82b702c064508056e39ad294cd155a0f
Shashank <keshavashashank@gmail.com>
2018-10-08 20:14:05 +0530

    Intial Commit

---
+++ packages/gatsby-starter-project/src/pages/index.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+import Layout from '../components/layout'
+
+const IndexPage = ({ data }) => {
+  const { edges: posts } = data.allMarkdownRemark
+  return (
+    <div>
+      {posts.map(({ node: post }) => {
+        const { frontmatter } = post
+
+        return (
+          <div>
+            <h2>
+              <Link to={frontmatter.path}>{frontmatter.title}</Link>
+            </h2>
+            <p>{frontmatter.date}</p>
+            <p>{frontmatter.excerpt}</p>
+          </div>
+        )
+      })}
+    </div>
+  )
+}
+
+export const query = graphql`
+  query IndexQuery {
+    allMarkdownRemark {
+      totalCount
+      edges {
+        node {
+          id
+          frontmatter {
+            title
+            date(formatString: "MMMM DD, YYYY")
+            path
+            tags
+            excerpt
+          }
+        }
+      }
+    }
+  }
+`
+export default IndexPage
