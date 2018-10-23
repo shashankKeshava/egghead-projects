230afc8c010ccff0be87cae54bcb5a2a67b8f9f6
Shashank K <Shashank_K@intuit.com>
2018-10-23 10:59:20 +0530

    Shallow test cases

--- packages/learning-enzyme-jest/src/App.test.js
+++ packages/learning-enzyme-jest/src/App.test.js
@@ -1,9 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import {configure, shallow} from "enzyme";
+import Adapter from "enzyme-adapter-react-16.3";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+configure({adapter: new Adapter()});
+
+describe("<App/>", () => {
+    const wrapper = shallow(<App />);
+    it("should contain 1 p element", () => {
+        expect(wrapper.find(".App-intro").exists()).toBe(true);
+    });
+    it("should contain 3 li element", () => {
+        expect(wrapper.find("ul").children().length).toBe(3);
+    });
 });
